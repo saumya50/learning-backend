5419b1920f02d75805fe0266e411ea945b971235
Saumya Shrestha <saumya.20242043@mnnit.ac.in>
2026-09-19 17:44:30 +0530

    refactor: remove redundant comment for secured routes in user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -17,6 +17,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// secured routes
 router.route("/register").post(
     upload.fields([
         {
@@ -32,8 +33,6 @@ router.route("/register").post(
 );
 
 router.route("/login").post(loginUser);
-
-// secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changePassword)
